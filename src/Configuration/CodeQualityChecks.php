1d5de79131a9cdf5f20ba9fcef8d19ac63c2ba27
Simon Praetorius <praetorius@sitegeist.de>
2020-07-28 11:33:10 +0200

    [TASK] Disable variable name check

--- src/Configuration/CodeQualityChecks.php
+++ src/Configuration/CodeQualityChecks.php
@@ -3,7 +3,8 @@
 use Sitegeist\FluidComponentsLinter\CodeQuality\Check;
 
 return [
-    Check\ComponentVariablesCheck::class,
+    // Disabled: This is more complicated due to f:variable, f:for...
+    //Check\ComponentVariablesCheck::class,
     Check\ParamNamingCheck::class,
     Check\ParamCountCheck::class,
     Check\ParamDescriptionCheck::class,
