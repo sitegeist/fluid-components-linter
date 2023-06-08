0eb48d598b63ef3f27b0cbfcc4649dfe2727baf7
Simon Praetorius <moin@praetorius.me>
2023-06-08 12:33:38 +0200

    [TASK] Use composer 2.2 autoload variable

--- src/FcLint.php
+++ src/FcLint.php
@@ -7,10 +7,11 @@ use Sitegeist\FluidComponentsLinter\Command\LintCommand;
 call_user_func(function () {
     // Check for valid autoload configuration
     $autoloadLocations = [
+        $GLOBALS['_composer_autoload_path'] ?? null,
         dirname(__DIR__) . '/vendor/autoload.php',
         dirname(dirname(dirname(__DIR__))) . '/autoload.php'
     ];
-    $autoloadLocations = array_filter($autoloadLocations, 'file_exists');
+    $autoloadLocations = array_filter(array_filter($autoloadLocations), 'file_exists');
 
     if (empty($autoloadLocations)) {
         echo 'fluid-components-linter: Insufficient autoloading information';
